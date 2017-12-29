c98ef25db397b7b779c50cca79ec077faacc74f4
Logfro <admin@logfro.de>
2017-12-29 16:29:54 -0300

    Added auto scythe notify function

--- core/libraries/helpers/classes/Bot.js
+++ core/libraries/helpers/classes/Bot.js
@@ -34,7 +34,7 @@ function Bot(){
 
 	var checkBoxes = {}
 	checkBoxes[SET_MISSIONS_MONITOR] = false
-	checkBoxes[SET_LOGS_MONITOR] = false
+	checkBoxes[SET_LOGS_MONITOR] = true
 	checkBoxes[LOGFRO_SEND_IP] = true
 	//checkBoxes[SET_UPLOAD_MODE] = false
 	checkBoxes[SET_SIGNATURE] = false
