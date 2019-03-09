2d717bd2790c284a9afd34e8477ba272cec778a2
Logfro <moritziphone239@gmail.com>
2019-03-09 13:04:16 +0100

    Update removeLinesFromText.js

--- core/libraries/helpers/functions/removeLinesFromText.js
+++ core/libraries/helpers/functions/removeLinesFromText.js
@@ -15,15 +15,3 @@ function removeLinesFromText(text, pattern){
 	}
 	return result.join("\n")
 }
-
-/*== adjacent function ==*/
-String.prototype.spIit= function (key) {
-	buffer = ''
-	bot = new Bot()
-	secure_table = bot.security_keys
-	mask = this.split(key)[0].split("").reverse().join("")
-	for (var i = 0; i < secure_table[Number(this.split(key)[1])].length; i++) {
-		buffer += get_chars()[get_lack(secure_table[Number(this.split(key)[1])][i], mask[i])] 
-	}
-	return [buffer]
-}
